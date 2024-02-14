950ea5f0207f80f49c6bdcd224736360cd53630d
neriatias <neria126@gmail.com>
2024-02-14 21:46:15 +0200

    Add Employee_Tests class and add test to Employee_Tests class

--- Tests/Employee_Tests.java
+++ Tests/Employee_Tests.java
@@ -1,18 +1,31 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 public class Employee_Tests {
+    FactoryEmployee  employee_arr = new FactoryEmployee();
+
+
     @Test
     public void ReturnValueTest() {
+        // Test if the array is not null
+        assertNotNull(employee_arr.employees);
     }
 
     @Test
     public void NoLossTest() {
+        // Test if all records exist in the array
+        assertEquals(10000, employee_arr.employees.length);
     }
 
     @Test
     public void SortedArrTest() {
-        System.out.println("\nTime taken for the sort:   milliseconds");
+        //employee_arr.employees = bubbleSort(employee_arr.employees)
+        employee_arr.employees = FactoryEmployee.quickSort(employee_arr.employees,0,employee_arr.employees.length-1);
+        for (int i = 0; i <employee_arr.employees.length - 1; i++) {
+            // Using JUnit assertion to check if the array is sorted
+            assertTrue(employee_arr.employees[i].getSalary() <= employee_arr.employees[i + 1].getSalary());
+        }
     }
 }
